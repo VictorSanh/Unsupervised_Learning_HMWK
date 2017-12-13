a4cec123faaf98b17c9b3d88f3ee3a33bf4edb63
Alexandre Attia <alexattia18@gmail.com>
2017-12-13 10:59:17 +0100

    Matrix LASSO Minimization by ADMM

---
+++ HMWK2/lasso_min.m
@@ -0,0 +1,24 @@
+function [C1] = lasso_min(data)
+N = size(data,2);
+% initialization
+A = inv(mu1*(data'*data)+mu2*eye(N));
+C1 = zeros(N,N);
+Lambda2 = zeros(N,N);
+err = 2*10^-3; 
+i = 1;
+while ( err(i) > thr1 && i < maxIter )
+    % updating Z
+    Z = A * (mu1*(data'*data)+mu2*(C1-Lambda2/mu2));
+    Z = Z - diag(diag(Z));
+    % updating C2
+    C2 = max(0,(abs(Z+Lambda2/mu2) - 1/mu2*ones(N))) .* sign(Z+Lambda2/mu2);
+    C2 = C2 - diag(diag(C2));
+    % updating Lambda
+    Lambda2 = Lambda2 + mu2 * (Z - C2);
+    % computing errors
+    err(i+1) = max(max(abs(Z-C2)));
+
+    C1 = C2;
+    i = i + 1;
+end
+end
