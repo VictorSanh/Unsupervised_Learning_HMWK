1606f0098501353f308f88a8946695833aad9571
VictorSanh <victorsanh@gmail.com>
2017-12-13 12:38:06 +0100

    K-Subspaces - Missing Replicates and exit condition

--- HMWK2/ksubspaces.m
+++ HMWK2/ksubspaces.m
@@ -5,27 +5,23 @@ function [global_groups, global_obj] = ksubspaces(data, n, d, replicates)
     % replicates: number of restarts
 
     [D,N] = size(data);
-
+    max_iter = 10;
+    
     % Initialization
     x = randperm(size(data,2),n);
     means = data(:,x);
     y = {} ;
-
     U = {} ;
-
     for i = 1:n
-
         temp = eye(D) ;%RandOrthMat(D);
         U{i} = temp(:, 1:d{i}); 
-
     end
 
-    max_iter = 2 ;
-
+    % EM
     for k = 1:max_iter
-
+        fprintf("iteration %i\n", k);
+        %Segmentation
         distance = zeros(n, N);
-
         for l = 1:n
             tmp = (eye(D) - U{l}*U{l}')*(data -  means(:,l));
             distance(l,:) = diag(tmp'*tmp);
@@ -37,15 +33,15 @@ function [global_groups, global_obj] = ksubspaces(data, n, d, replicates)
             W(idx(j), j) = 1;
         end
         
+        %Estimation
         for i = 1:n
-            
             num = sum(repmat(W(i,:),D,1).*data, 2);
             dem = sum(W(i,:));
             means(:,i) = num/dem ;
             
             z = repmat(W(i,:), D, 1).*(data - means(:,i));
             [V, E] = eig(z*z');
-            [eigen_values_sorted, reorder] = sort(diag(E));
+            [~, reorder] = sort(diag(E));
             U{i} = V(:,reorder(1:d{i})); 
             
             for j = 1:N
@@ -53,14 +49,10 @@ function [global_groups, global_obj] = ksubspaces(data, n, d, replicates)
                     y{j} = U{i}'*(data(:,j) - means(:,i));
                 end
             end
-            
-                
         end
-
-
     end
 
 
-    global_groups = 0;
-    global_obj = 0;
+    [~ , global_groups] = max(W);
+    global_obj = {y, U};
 end
