11844c82148e028c9fb6c09a684e50b86fe50135
Alexandre Attia <alexattia18@gmail.com>
2017-12-13 09:55:47 +0100

    build laplacian and spectracl clustering

---
+++ HMWK2/build_laplacian.m
@@ -0,0 +1,31 @@
+function [L] =  build_laplacian(Affinity, laplacian_normalization)
+%  [L] =  build_laplacian(X, graph_param, laplacian_normalization)
+%      a skeleton function to construct a laplacian from data,
+%      needs to be completed
+%
+%  Input
+%  X:
+%      Affinity: N by N affinity matrix, where N is the number of points.
+%  laplacian_normalization:
+%      string selecting which version of the laplacian matrix to construct
+%      either 'unn'normalized, 'sym'metric normalization
+%      or 'rw' random-walk normalization
+%
+%  Output
+%  Y:
+%      Cluster assignments
+
+W = Affinity;
+
+D = diag(sum(W,2));
+
+if strcmp(laplacian_normalization, 'unn')
+    L = D - W;
+elseif strcmp(laplacian_normalization, 'sym')
+    L = inv(D)^(1/2)*(D - W)*inv(D)^(1/2);
+elseif strcmp(laplacian_normalization, 'rw')
+    L = eye(size(W)) - inv(D)*W;
+else
+    error('unkown normalization mode')
+end
+
---
+++ HMWK2/spectral_clustering.m
@@ -0,0 +1,27 @@
+function [Y] = spectral_clustering(Affinity, num_classes)
+%  [Y] = spectral_clustering(L, chosen_eig_indices, num_classes)
+%      a skeleton function to perform spectral clustering, needs to be completed
+%
+%  Input
+%  L:
+%      Graph Laplacian (standard or normalized)
+%  num_classes:
+%      number of clusters to compute (defaults to 2)
+%
+%  Output
+%  Y:
+%      Cluster assignments
+
+L =  build_laplacian(Affinity, laplacian_normalization);
+
+if nargin < 3
+    num_classes = 2;
+end
+
+[U,E] = eig(L);
+
+[eigenvalues_sorted,reorder] = sort(diag(E));
+
+U = U(:,reorder(num_classes));
+
+Y = kmeans(U, num_classes);
